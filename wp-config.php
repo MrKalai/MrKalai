7a4578a8c5568e0453a2d24acbc902edbf83c6e1
KalaiMr <tamilmenutech@gmail.com>
2020-08-09 15:38:02 +0530

    3.37

--- wp-config.php
+++ wp-config.php
@@ -65,6 +65,18 @@ define( 'NONCE_SALT',       'tIH|V)qOu)[9>^_u5Gx#Wv7y%@zjw@v~@!J~^<#GoZ;GS<|0M.F
  */
 $table_prefix = 'wp_';
 
+  define('WP_DEBUG', false); 
+  define('WP_HTTP_BLOCK_EXTERNAL', false);
+  define('FS_METHOD', 'direct');
+  define('DISABLE_WP_CRON', true); 
+  define('WP_MEMORY_LIMIT', '256M');
+  define( 'WP_CACHE', true );
+  define('WP_ALLOW_REPAIR', true);
+  require_once(ABSPATH . 'wp-settings.php');
+  add_filter( 'auto_update_plugin', '__return_false' ); 
+  add_filter( 'auto_update_theme', '__return_false' ); 
+  define( 'WP_AUTO_UPDATE_CORE', 'major' );
+
 /**
  * For developers: WordPress debugging mode.
  *
